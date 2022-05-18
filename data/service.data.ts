96e94ea5965bffc29805873b094f2366c8fb09fa
Zelal Hossain <zelal@ojjomedia.com>
2022-05-18 08:20:01 +0600

    Create service.data.ts

---
+++ data/service.data.ts
@@ -0,0 +1,37 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export interface serviceMenuDocument {
+  _id: string
+  name: string
+  description: string
+  slug: string
+}
+
+export const serviceMenuData = [
+  {
+    id: uuidv4(),
+    title: 'MERN Stack Website Design',
+    description: 'We can help you to develop your website using MERN Stack',
+    slug: 'mern-stack-website-development',
+  },
+  {
+    id: uuidv4(),
+    title: 'Frontity Website Development',
+    description: 'Create lightning fast websites using WordPress and React.',
+    slug: 'frontity-website-development',
+  },
+
+  {
+    id: uuidv4(),
+    title: 'Convert Design to Pixel Perfect Code',
+    description:
+      'We can help you to convert your design to pixel perfect code.',
+    slug: 'convert-design-to-code',
+  },
+  {
+    id: uuidv4(),
+    title: 'WordPress Website Development',
+    description: 'We can help you to develop your website using MERN Stack',
+    slug: 'wordpress-website-development',
+  },
+]
