14470d6a46d79a5ae2be2bffb7c21ebc67b6747c
Zelal Hossain <zelal@ojjomedia.com>
2022-05-29 13:02:46 +0600

    Update AdminNavbar.tsx

--- src/components/Navbar/AdminNavbar.tsx
+++ src/components/Navbar/AdminNavbar.tsx
@@ -4,18 +4,31 @@ import React from 'react'
 import { BsList } from 'react-icons/bs'
 import { HiBell, HiMail } from 'react-icons/hi'
 
-const AdminNavbar = ({handleSidebar}:{handleSidebar:any}) => {
+const AdminNavbar = ({ handleSidebar }: { handleSidebar: any }) => {
   return (
-    <div className='bg-white flex items-center shadow-md justify-between px-5 py-3 relative'>
-        <div>
-            <Link href="/"><a><Image src="/brandLogo.svg" alt='' width="136" height="42" /></a></Link>
-        </div>
-        <div className=' items-center lg:flex hidden'>
-            <button><HiMail className='text-xl text-gray-500 mr-5' /></button>
-            <button><HiBell className='text-xl text-gray-500 mr-8' /></button>
-            <div className='w-[2.25rem] h-[2.25rem] rounded-full bg-blueTwo' />
-        </div>
-        <div className='p-3 rounded-md bg-gradient-to-r from-blueOne to-blueTwo shadow-3xl lg:hidden block cursor-pointer' onClick={handleSidebar}><BsList  style={{color:"white", width:"18px", height:"15.5px"}}/></div>
+    <div className="bg-white flex items-center shadow-md justify-between px-5 py-3 relative">
+      <div>
+        <Link href="/">
+          <a>
+            <Image src="/brandLogo.svg" alt="" width="136" height="42" />
+          </a>
+        </Link>
+      </div>
+      <div className="items-center lg:flex hidden">
+        <button>
+          <HiMail className="text-xl text-gray-500 mr-5" />
+        </button>
+        <button>
+          <HiBell className="text-xl text-gray-500 mr-8" />
+        </button>
+        <div className="w-[2.25rem] h-[2.25rem] rounded-full bg-blueTwo" />
+      </div>
+      <div
+        className="p-3 rounded-md bg-gradient-to-r from-blueOne to-blueTwo shadow-3xl lg:hidden block cursor-pointer"
+        onClick={handleSidebar}
+      >
+        <BsList style={{ color: 'white', width: '18px', height: '15.5px' }} />
+      </div>
     </div>
   )
 }
