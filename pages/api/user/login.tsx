cff324af172ddb7380faea277d11aa799ea6f6c5
Zelal Hossain <zelal@ojjomedia.com>
2022-06-22 16:58:42 +0600

    Update login.tsx

--- pages/api/user/login.tsx
+++ pages/api/user/login.tsx
@@ -1,32 +1,47 @@
+import { result } from 'lodash'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function name(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST')
     return res.status(400).json({ message: 'You are not allowed' })
 
-  // request login access to wordpress route /wp-json/jwt-auth/v1/token
-  const response = await fetch(
-    `${process.env.API_ENDPOINT}/wp-json/jwt-auth/v1/token`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Basic ${process.env.CONSUMER_TOKEN}`,
-      },
-      body: JSON.stringify({
-        username: req.body.email,
-        password: req.body.password,
-      }),
-    }
-  )
-  const data = await response.json()
-  if (data.status === 'error') {
-    return res.status(400).json({ message: data.message })
+  // var formdata = new FormData()
+  // formdata.append('username', 'jstemplate')
+  // formdata.append('password', 'VtQbxn9Rz$Bt^fDsGm')
+
+  var requestOptions = {
+    method: 'POST',
   }
+
+  fetch(
+    'https://api-blog.jstemplate.net/wp-json/jwt-auth/v1/token?username=jstemplate&password=VtQbxn9Rz$Bt^fDsGm',
+    requestOptions as any
+  )
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log('error', error))
+
+  // // request login access to wordpress route /wp-json/jwt-auth/v1/token
+  // const response = await fetch(
+  //   `${process.env.API_ENDPOINT}/wp-json/jwt-auth/v1/token?username=jstemplate&password=VtQbxn9Rz$Bt^fDsGm`,
+  //   {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       Authorization: `Basic ${process.env.CONSUMER_TOKEN}`,
+  //     },
+  //   }
+  // )
+  console.log(`${process.env.API_ENDPOINT}/wp-json/jwt-auth/v1/token`)
+  console.log('req.body ', JSON.stringify(req.body))
+  // const data = await response.json()
+  // if (data.status === 'error') {
+  //   return res.status(400).json({ message: data.message })
+  // }
 
   // return token to client
   return res.status(200).send({
     message: 'Successfully fetched token',
-    accessToken: data,
+    accessToken: result,
   })
 }
