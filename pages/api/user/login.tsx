6a817a94aad0932a13d32a504f4e00930967503e
Zelal Hossain <zelal@ojjomedia.com>
2022-07-13 23:20:02 +0600

    Update login.tsx

--- pages/api/user/login.tsx
+++ pages/api/user/login.tsx
@@ -1,5 +1,6 @@
-import axios from 'axios'
-import { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios';
+import { setCookie } from 'cookies-next';
+import { NextApiRequest, NextApiResponse } from 'next';
 
 
 export default async function ( req: NextApiRequest, res: NextApiResponse ) {
@@ -9,7 +10,7 @@ export default async function ( req: NextApiRequest, res: NextApiResponse ) {
   try {
     // call wordpress login api and get token using axios
     const { data } = await axios.post(
-      `${ process.env.API_ENDPOINT }/wp-json/jwt-auth/v1/token`,
+      `${process.env.API_ENDPOINT}/wp-json/jwt-auth/v1/token`,
       {
         username: req.body.username,
         password: req.body.password,
@@ -20,6 +21,14 @@ export default async function ( req: NextApiRequest, res: NextApiResponse ) {
         },
       }
     )
+
+    // extract the token from data
+    const { token } = data.data
+
+    // set token to cookie
+
+    setCookie( 'token', token, { req, res, maxAge: 60 * 60 * 24 } )
+
     // return response
     return res.status( 200 ).send( {
       message: 'Successfully logged in',
