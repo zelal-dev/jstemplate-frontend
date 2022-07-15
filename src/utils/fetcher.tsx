c8078d87b1d1cbac1654e3ff9b9ca234435b941a
Zelal Hossain <zelal@ojjomedia.com>
2022-07-15 22:19:50 +0600

    Create fetcher.tsx

---
+++ src/utils/fetcher.tsx
@@ -0,0 +1,13 @@
+import { Axios, SeoAxios } from './axiosKits'
+
+export const fetcher = ( url: string ) => Axios( url ).then( ( res ) => res.data.data )
+
+export const seoFetcher = ( url: string ) => SeoAxios( url ).then( ( res ) => res.data )
+
+export const seoPageFetcher = async ( slug: string ) => {
+  //  call seo fetcher
+  // fetch seo data from wp rankmath plugin
+  return await fetch( `${process.env.NEXT_PUBLIC_API_ENDPOINT}/wp-json/rankmath/v1/getHead?url=${process.env.NEXT_PUBLIC_API_ENDPOINT}/${slug}` ).then( ( res ) => res.json() )
+
+}
+
